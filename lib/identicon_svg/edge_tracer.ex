b0b457b422c1cfa6382f7272a570f6de23d0fa91
Isaak Tsalicoglou <isaak@waseigo.com>
2024-05-07 17:44:17 +0300

    EdgeTracer now works with edge midpoints, reliably extracting all edgesets per polygon

--- lib/identicon_svg/edge_tracer.ex
+++ lib/identicon_svg/edge_tracer.ex
@@ -1,4 +1,82 @@
 defmodule IdenticonSvg.EdgeTracer do
+  alias IdenticonSvg.PolygonReducer
+
+  # identicon_edges is the :edges field of an %Identicon{}, and represents
+  # a list of polygon edgelists already extracted by PolygonReducer.group/1
+  def doit(identicon_edges) do
+    Enum.map(identicon_edges, &doit_one/1)
+  end
+
+  # polygon_edges is a list element of identicon_edges (the argument to doit/1)
+  def doit_one(polygon_edges) do
+    # first, get the midpoints
+    midpoints = Enum.map(polygon_edges, &midpoint/1)
+
+    # then, generate all theoretical neighbors per midpoint;
+    # not all of them actually exist in the polygon
+    theoretical_neighbors =
+      midpoints
+      |> Enum.map(&{&1, edge_neighbors_by_midpoint(&1)})
+      |> Map.new()
+
+    # next, filter the theoretical neighbors of each present edge's midpoint and keep only
+    # those that actually exist in the polygon
+    actual_neighbors_per_edge =
+      theoretical_neighbors
+      |> Enum.map(fn {midpoint, neighbors} ->
+        {
+          midpoint,
+          MapSet.intersection(MapSet.new(midpoints), MapSet.new(neighbors))
+          |> MapSet.to_list()
+        }
+      end)
+      |> Map.new()
+
+    # use PolygonReducer's core function to group edges together, thus identifying any
+    # disjointed edgelists, such as those representing "donut holes" and the exterior
+    # hull of the polygon; we will need to bridge these later
+    edge_sublists = PolygonReducer.group(actual_neighbors_per_edge)
+
+    # For each of the edge sublists, generate a connectivity map; the result is a list
+    # of those sublists' connectivity maps
+    connectivity_maps =
+      edge_sublists
+      |> Enum.map(fn edge_sublist ->
+        Enum.map(
+          edge_sublist,
+          fn midpoint ->
+            {midpoint, Map.get(actual_neighbors_per_edge, midpoint)}
+          end
+        )
+        |> Map.new()
+      end)
+
+    # For each connectivity map:
+    # 1. Connect its edges (in the form of edge midpoints)
+    # 2. Convert each midpoint to an unoriented edge in [{a, b}, {c, d}] notation
+    # 3. Trace them in a consistent direction using direct/1
+    # 4. The result is one list of traced and directed edges per connectivity map
+
+    # directed_traced_edgelists =
+    connectivity_maps
+    |> Enum.map(fn cm when is_map(cm) ->
+      dt =
+        cm
+        |> connect_edges()
+        |> Enum.map(fn mp -> midpoint_to_unoriented_edge(mp) end)
+        |> Enum.reverse()
+
+      Enum.reduce(
+        tl(dt),
+        [hd(dt)],
+        fn x, acc ->
+          [direct(x, hd(acc)) | acc]
+        end
+      )
+      |> Enum.reverse()
+    end)
+  end
+
   def connect(polygon_edges)
       when is_list(polygon_edges) and polygon_edges != [] do
     connect(polygon_edges, [])
@@ -36,7 +114,7 @@ defmodule IdenticonSvg.EdgeTracer do
     fully_connected
   end
 
-  def direct([_a, b] = current, [c, _d] = _previous) do
+  def direct([c, _d] = current, [_a, b] = _previous) do
     if b == c do
       current
     else
@@ -198,7 +276,7 @@ defmodule IdenticonSvg.EdgeTracer do
     Enum.map(vector, &div(&1, magnitude))
   end
 
-  def point_plus_vector({xp, yp} = point, [xv, yv] = vector) do
+  def point_plus_vector({xp, yp} = _point, [xv, yv] = _vector) do
     {xp + xv, yp + yv}
   end
 
@@ -212,7 +290,7 @@ defmodule IdenticonSvg.EdgeTracer do
     |> Enum.reverse()
   end
 
-  def edge_orientation_by_midpoint({mpx, mpy} = _midpoint) do
+  def edge_orientation_by_midpoint({mpx, mpy} = _midpoint) when mpx != mpy do
     rmpx = round(mpx) == mpx
     rmpy = round(mpy) == mpy
 
@@ -223,7 +301,7 @@ defmodule IdenticonSvg.EdgeTracer do
     end
   end
 
-  def edge_neighbors_by_midpoint({mpx, mpy} = midpoint) do
+  def edge_neighbors_by_midpoint({mpx, mpy} = midpoint) when mpx != mpy do
     case edge_orientation_by_midpoint(midpoint) do
       :horizontal ->
         [
@@ -247,5 +325,104 @@ defmodule IdenticonSvg.EdgeTracer do
         ]
         |> Enum.map(&point_plus_vector(midpoint, &1))
     end
+  end
+
+  def midpoint_to_unoriented_edge({mpx, mpy} = midpoint) when mpx != mpy do
+    case edge_orientation_by_midpoint(midpoint) do
+      :horizontal ->
+        [
+          [mpx - 0.5, mpy],
+          [mpx + 0.5, mpy]
+        ]
+
+      :vertical ->
+        [
+          [mpx, mpy + 0.5],
+          [mpx, mpy - 0.5]
+        ]
+    end
+    |> Enum.map(fn mp_edge ->
+      Enum.map(
+        mp_edge,
+        fn coord -> floor(coord) end
+      )
+      |> List.to_tuple()
+    end)
+  end
+
+  def connect_edges(s) when is_map(s) and s != %{} do
+    acc0 = %{routed: [], candidates: s}
+
+    Enum.reduce_while(
+      0..length(Map.keys(s)),
+      acc0,
+      fn _iteration, acc ->
+        %{candidates: candidates} = acc
+
+        if candidates != %{} do
+          {:cont, edge_connector(acc)}
+        else
+          {:halt, acc}
+        end
+      end
+    )
+    |> Map.get(:routed)
+
+    # |> perhaps_close_loop()
+  end
+
+  def edge_connector(%{routed: [], candidates: candidates} = _acc)
+      when candidates != %{} do
+    next_edge = candidates |> Map.keys() |> hd()
+    routed = [next_edge]
+    candidates_new = candidates |> Map.delete(next_edge)
+
+    %{
+      routed: routed,
+      candidates: candidates_new
+    }
+  end
+
+  def edge_connector(%{routed: routed, candidates: candidates} = _acc) do
+    next_edge =
+      candidates
+      |> Map.filter(fn {_k, v} -> hd(routed) in v end)
+      |> Map.keys()
+      |> hd()
+
+    routed_new = [next_edge | routed]
+    candidates_new = Map.delete(candidates, next_edge)
+
+    %{
+      routed: routed_new,
+      candidates: candidates_new
+    }
+  end
+
+  def edge_connector(%{candidates: []} = acc) do
+    acc
+  end
+
+  def perhaps_close_loop(routed) when is_list(routed) do
+    last_point = routed |> tl() |> Enum.reverse() |> hd()
+
+    if last_point != hd(routed) do
+      [last_point | routed]
+    else
+      routed
+    end
+  end
+
+  def trace_edges(routed) when is_list(routed) do
+    Enum.reduce(
+      tl(routed),
+      [hd(routed)],
+      fn edge, traces ->
+        IO.inspect(traces)
+        IO.puts("\n")
+        [direct(edge, hd(traces)) | traces]
+      end
+    )
+    |> Enum.reverse()
   end
 end
