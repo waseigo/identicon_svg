98ea48ea878beebf9e63f04c2555e83a93b83c9c
Isaak Tsalicoglou <isaak@waseigo.com>
2024-06-14 00:43:29 +0300

    removed unnecessary `require EEx` line

--- lib/identicon_svg/color.ex
+++ lib/identicon_svg/color.ex
@@ -2,8 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 defmodule IdenticonSvg.Color do
-  require EEx
-
   @moduledoc """
   Module of `IdenticonSvg` that contains functions related to handling colors.
   """
